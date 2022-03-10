ead904fa281f46b81bf68d1c6ab141d26ec4c694
hhtboy <hhtboy1024@gmail.com>
2022-03-10 22:51:32 +0900

    grabObject.cs 수정
    물제를 잡을 수 있는지 isGrabable추가.(오브젝트와 플레이어 거리로 판단)
    물제를 잡을 수 있으면 하이라이트 되는 기능 추가.
    물체를 잡으면 물체의 collider가 비활성화.

--- Team2_proj/Assets/Scripts/grabObject.cs
+++ Team2_proj/Assets/Scripts/grabObject.cs
@@ -6,29 +6,103 @@ using UnityEngine;
 
 public class grabObject : MonoBehaviour
 {
-    
     public Transform theDest;
 
-    private Rigidbody rigid;
+    private Rigidbody myRigid;
     private Collider myCollider;
+    private MeshRenderer myRenderer;
+    private float objDistance;
 
-    void OnMouseDown()
+    private bool isGrab;
+    private bool isGrabable;
+ 
+
+
+    void Start()
     {
-        rigid = GetComponent<Rigidbody>();
-        myCollider = GetComponent<Collider>();
-
-        rigid.useGravity = false;
-        myCollider.enabled = false;
-        
-        transform.position = theDest.position;
-        transform.parent = GameObject.Find("GrabDest").transform;
+        isGrab = false;
+  
     }
+
+    void Update()
+    {
+        //마우스를 클릭하고 있으면 오브젝트가 highlight되지 않음
+        isGrab = Input.GetMouseButton(0);
+        distanceCulculate();
+    }
+
+
+    void OnMouseDown() //
+    {
+        if(isGrabable)
+        {
+            myRigid = GetComponent<Rigidbody>();
+            myCollider = GetComponent<Collider>();
+
+
+            myRigid.useGravity = false;
+            myCollider.enabled = false;
+
+
+            transform.position = theDest.position;
+            transform.parent = GameObject.Find("GrabDest").transform;
+        }
+        
+
+        
+    }
+    
 
     void OnMouseUp()
     {
-        transform.parent = null;
-        rigid = GetComponent<Rigidbody>();
-        rigid.useGravity = true;
+        myRigid = GetComponent<Rigidbody>();
+
+        myRigid.useGravity = true;
         myCollider.enabled = true;
+        transform.parent = null;
+
+        isGrab = false;
     }
+
+    void OnMouseOver()
+    {
+        myRenderer = GetComponent<MeshRenderer>();
+
+        if ((!isGrab)&&(isGrabable)) //잡고 있을 때는 색 변경 X
+        {
+            myRenderer.material.color = new Color(1, 0, 0);
+        }
+
+        if(!isGrabable)
+        {
+            myRenderer.material.color = new Color(1, 1, 1);
+        }
+
+        
+    }
+
+    void OnMouseExit()
+    {
+        myRenderer.material.color = new Color(1, 1, 1);
+    }
+
+   
+    void distanceCulculate() //물제와 플레이어 사이의 거리를 측정 후 잡을 수 있는지 판단
+    {
+        Vector3 _objPos = transform.position;
+        Vector3 _playerPos = GameObject.Find("Player").GetComponent<PlayerMovement>().transform.position;
+        objDistance = Vector3.Distance(_objPos, _playerPos);
+
+        if (objDistance <= 3.0f)
+        {
+            isGrabable = true;
+        }
+        else
+        {
+            isGrabable = false;
+        }
+
+    }
+
+
 }
