210f209860de1d63aed190d8b4b63fc823999656
Pumandu <100417602+Pumandu@users.noreply.github.com>
2022-03-14 22:43:23 +0900

    틀린그림찾기 수정했습니다.

--- Team2_proj/Assets/Scripts/FindWrongPicture.cs
+++ Team2_proj/Assets/Scripts/FindWrongPicture.cs
@@ -22,7 +22,7 @@ public class FindWrongPicture : MonoBehaviour
     public void SolveQuiz(Vector3 localPoint)
     {
         //틀린 부분에 해당하는 좌표에 raycast가 충돌한 상태로 마우스 왼쪽 클릭을 하면 이벤트 발생
-        if((localPoint.x <= -0.5 || localPoint.x >= -0.54)  && (localPoint.y >= 1.43 || localPoint.y <= 1.51))
+        if((localPoint.x <= 0.44 && localPoint.x >= 0.39)  && (localPoint.y >= 0.06 && localPoint.y <= 0.13))
         {
             if (Input.GetMouseButtonDown(0))
             {
