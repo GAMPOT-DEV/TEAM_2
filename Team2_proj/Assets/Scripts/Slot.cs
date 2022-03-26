095f68065e50ca7ae0a21743163d55d168c967ab
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-03-26 22:54:38 +0900

    설정창 UI 이미지 추가

--- Team2_proj/Assets/Scripts/Slot.cs
+++ Team2_proj/Assets/Scripts/Slot.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
     public Item item;
     public int ItemCount;
     public Image itemImage;
+
     void Start()
     {
         
