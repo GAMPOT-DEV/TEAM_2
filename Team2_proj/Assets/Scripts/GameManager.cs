ed914f4afd07b19f1245d227bb120a4dd3250eaa
hhtboy <hhtboy1024@gmail.com>
2022-04-01 11:12:39 +0900

    Main Screen 추가

--- Team2_proj/Assets/Scripts/GameManager.cs
+++ Team2_proj/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -35,4 +36,6 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1f;
         PausePanel.SetActive(false);
     }
+
+    
 }
