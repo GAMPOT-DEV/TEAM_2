7c00f888f0a454dc7d2b6e46fe816509f64f1508
hhtboy <hhtboy1024@gmail.com>
2022-04-05 21:50:28 +0900

    Red book 복사 버그 수정

--- Team2_proj/Assets/Scripts/RedBook.cs
+++ Team2_proj/Assets/Scripts/RedBook.cs
@@ -7,14 +7,17 @@ public class RedBook : MonoBehaviour
     public bool isClicked;
     public GameObject paper;
 
+    private bool alreadyClear;
+
     void Start() {
         isClicked = false;
+        alreadyClear = false;
     }
     void Update() {
         {
-            if(isClicked)
+            if(isClicked&&!alreadyClear)
             {
-                
+                alreadyClear = true;
                 Vector3 rot = new Vector3(transform.rotation.x+90, transform.rotation.y, transform.rotation.z+10);
                 isClicked = false;
                 Instantiate(paper, transform.position + new Vector3(1, -0.7f, 0), Quaternion.Euler(rot));
