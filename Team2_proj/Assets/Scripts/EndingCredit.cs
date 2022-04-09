77f14ff425869d6e2fca264264d57a661cd958f7
hhtboy <hhtboy1024@gmail.com>
2022-04-10 03:14:35 +0900

    엔딩 크레딧

---
+++ Team2_proj/Assets/Scripts/EndingCredit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCredit : MonoBehaviour
+{
+    void Update()
+    {
+        if(transform.position.y >= 7500)
+        {
+            Application.Quit(); // 어플리케이션 종료
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y+0.5f, transform.position.z);
+    }
+}
