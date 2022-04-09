d8b70c9c8f2b09207f215b00b3d0893fcef9333d
hhtboy <hhtboy1024@gmail.com>
2022-04-09 23:04:22 +0900

    씬에 폰트 적용, 메인스크린 fadeout 수정

--- Team2_proj/Assets/Scripts/MainScreen.cs
+++ Team2_proj/Assets/Scripts/MainScreen.cs
@@ -17,14 +17,25 @@ public class MainScreen : MonoBehaviour
         float deltaTime = 0f;
 
         GameObject panel = GameObject.Find("Canvas/MainScreen/Panel");
+        GameObject img = GameObject.Find("Canvas/MainScreen/Button/Image");
+        GameObject title = GameObject.Find("Canvas/MainScreen/Title_name");
+
         Color panelColor = panel.GetComponent<Image>().color;
+        Color imgColor = img.GetComponent<Image>().color;
+        Color titleColor = title.GetComponent<Image>().color;
 
 
         while (deltaTime<=2.0f)
         {
             deltaTime += 0.02f;
             panelColor.a += 0.01f;
+            imgColor.a -= 0.01f;
+            titleColor.a -= 0.01f;
+
             panel.GetComponent<Image>().color = panelColor;
+            img.GetComponent<Image>().color = imgColor;
+            title.GetComponent<Image>().color = titleColor;
+
             yield return new WaitForSeconds(0.02f);
         }
         SceneManager.LoadScene("FantasyRoom");
